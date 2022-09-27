a58417ff3a2ba2649ee5c69043695de3813e4267
Martin3737 <114466446+Martin3737@users.noreply.github.com>
2022-09-27 08:46:17 +0200

    init
    Hello from Tomas

---
+++ -Hellowrod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulačka1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Sqrt(num1));
+            Console.ReadKey();
+        }
+    }
+}
